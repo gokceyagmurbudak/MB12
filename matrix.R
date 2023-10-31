b18600af3cee1c075587123ce9ccee72c5602a8f
gokceyagmurbudak <gokcebudak51@gmail.com>
2023-10-31 11:45:06 +0100

    my first commit :)

---
+++ matrix.R
@@ -0,0 +1,10 @@
+m1 <- matrix(c(1,2,3,4,5,6),nrow=2)
+m1
+m2 <- matrix(c(7,8,9,10,11,12),
+             nrow=2,
+             ncol=3,
+             byrow=TRUE)
+m2
+m1 [ , 2 ]
+m1 [ 2 , ]
+m1 [ 2 , 2 ] 
